767846119a64dca42d616feeaada74b55bd37f59
Coco <143611879+cfrikke@users.noreply.github.com>
2023-10-30 13:16:57 +0000

    done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class Poggers{
+
+
+public static void Main(string[] args)
+{
+        GreetUser("Shasha Wu"); // Displays “Hello, Shasha Wu”
+        // call your functions below:
+	RectArea(69, 420);
+
+
+
+}
+
+public static void GreetUser(string name) {
+    Console.WriteLine("Hello " + name + ".");
+}
+
+public static void RectArea(double length, double width) {
+Console.WriteLine(length*width);
+}
+
+}
